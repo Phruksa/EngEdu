a53641a3f760b760a4616d643b602aed6222c8c3
Phruksa <phruksa4165@gmail.com>
2019-09-06 02:34:21 +0700

    3rd year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -14,6 +14,7 @@ namespace EngEdu
             Console.WriteLine("Year 2561 semester 2, I learn to program in C sharp.");
             Console.WriteLine("Year 2562 semester 1, I start my programmer life.");
             Console.WriteLine("Year 2562 semester 2, I think learn to digital.");
+            Console.WriteLine("Year 2563 semester 1, I will learn to make application.");
 
         }
     }
