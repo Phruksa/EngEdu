fcc4368b1b094c6f2523bc80879a534d1e242669
Phruksa <phruksa4165@gmail.com>
2019-09-06 03:01:48 +0700

    4th year semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -17,6 +17,7 @@ namespace EngEdu
             Console.WriteLine("Year 2563 semester 1, I prepare for apprentice.");
             Console.WriteLine("Year 2563 semester 2, I have to do apprentice.");
             Console.WriteLine("Year 2564 semester 1, I will make to project.");
+            Console.WriteLine("Year 2564 semester 2, I prepare the student teaching practice.")
         }
     }
 }
