bdf86a0b77a6443ca3be9aa43cc5c2faaca53974
Phruksa <phruksa4165@gmail.com>
2019-09-06 02:15:47 +0700

    2nd year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -11,7 +11,9 @@ namespace EngEdu
         static void Main(string[] args)
         {
             Console.WriteLine("Year 2561 semester 1, I am an Engedu freshman.");
-            Console.WriteLine("Year 2561 semester 1, I learn to program in C sharp");
+            Console.WriteLine("Year 2561 semester 2, I learn to program in C sharp.");
+            Console.WriteLine("Year 2562 semester 1, I have to regrade on some course.");
+
         }
     }
 }
